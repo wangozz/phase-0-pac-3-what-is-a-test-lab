2eda72d32441a187be1e1eaa403b2dd12fac4111
SamuelWango <wangos478@gmail.com>
2023-06-14 20:32:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+const Name = "Susan";
+const Height = 25;
+const Message = `${Name} is ${Height} inches tall`;
+
+console.log(message,
+    Message);
